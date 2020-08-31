19263f024167c86df65f047d7822bb49295f9b52
KwonSunwon <kso00011147@gmail.com>
2020-08-31 11:51:56 +0000

    Bool

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+void Print_int(int x){
+	printf("%d 인수는 %s입니다.\n", x, (x)? "참":"거짓");
+}
+void Print_bool(bool x){
+	std::cout << std::boolalpha << x << " 인수는 " << (x?"참":"거짓") << "입니다." << std::endl;
+	std::cout << "bool 타입의 데이터는 " << std::boolalpha << x << "와" << !x <<"로 표시할 수 있습니다." << std::endl;
+}
+
 int main(){
-	std::cout << "Hello, World" << std::endl;
+	bool ok = true;
+	bool notok = false;
+	
+	Print_int(100);
+	Print_bool(ok);
+	
 	return 0;
 }
