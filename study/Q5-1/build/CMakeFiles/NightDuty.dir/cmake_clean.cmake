e22b50f3d487630952dae15d3ffcee0d44d31733
kwonsunwon <kso00011147@gmail.com>
2020-12-27 06:10:13 +0000

    sagmentation fault (core dumped)

---
+++ study/Q5-1/build/CMakeFiles/NightDuty.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NightDuty.dir/CLinkedList.c.o"
+  "CMakeFiles/NightDuty.dir/NightDutyMain.c.o"
+  "NightDuty"
+  "NightDuty.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NightDuty.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
